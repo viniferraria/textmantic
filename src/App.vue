7d128a7dce690d826bac12244dbf4d328b6d1cb7
viniferraria <vferraria10@gmail.com>
2020-06-15 10:19:05 -0300

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img title="eid sucks" alt="Textmantic logo" src="./assets/logo.gif">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TextField></TextField>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld'
+import TextField from './components/TextField'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    TextField
   }
 }
 </script>
---
+++ src/components/TextField.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>
+            <textarea :v-model="text"/>
+        </div>
+        <div>
+            <textarea/>
+        </div>
+        <button>Send</button>
+    </div>
+</template>
+
+<script>
+export default {
+	name: "TextField",
+	data() {
+		return {
+			text: "",
+		};
+	},
+};
+</script>
+
+<style>
+
+</style>
